b0e6da39314edec5fb1ebcc605298d9efd813458
paolamastaiza@gmail.com <paolamastaiza@gmail.com>
2014-04-16 19:26:07 +0100

    initialization

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+$(window).load(function() {
+
+ $("#slider1").responsiveSlides({
+        auto: false,
+        pager: true,
+        nav: true,
+        speed: 500,
+        maxwidth: 800,
+        namespace: "centered-btns"
+      });
+
+
+});
